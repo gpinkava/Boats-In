9ee9bb2517295ba00d1d6ce7252d6b6f2cf5bda8
Glen A. Pinkava <gpinkava@gmail.com>
2017-06-01 23:57:16 -0400

    Initial Commit

---
+++ Boats In/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Boats In
+//
+//  Created by Glen Anthony on 6/1/17.
+//  Copyright © 2017 Glen Anthony. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
